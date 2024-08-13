c68929f63670674feb76f8669905efbbccb18673
Hoofard <Ryan.Hoofard@arb.ca.gov>
2024-08-13 14:04:17 -0700

    feat: added new role and policy

--- providers.tf
+++ providers.tf
@@ -1,11 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "ryanh-github-oidc-terraform-test"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-  }
+  backend "s3" {}
 }
 
-provider "aws" {
-  region = "us-west-2"
-}
+provider "aws" {}
